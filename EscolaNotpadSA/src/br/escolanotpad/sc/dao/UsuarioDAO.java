31f7f75e11f603af4c7e717938dd9310c1eadacf
Leonardo Bertol <leonardo_bertol@estudante.sc.senai.br>
2016-04-20 21:11:42 -0300

    Criação do UsuarioDAO Criação do Usuário(Apenas para não apresentar erro no UsuarioDAO)

---
+++ EscolaNotpadSA/src/br/escolanotpad/sc/dao/UsuarioDAO.java
@@ -0,0 +1,36 @@
+package br.escolanotpad.sc.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.escolanotpad.sc.commons.JpaUtil;
+import br.escolanotpad.sc.model.entity.Usuario;
+
+public class UsuarioDAO {
+	
+	public void salvar(Usuario usuario){
+		getEM().merge(usuario);
+	}
+	
+	public Usuario buscarPorId(Long id){
+		return getEM().find(Usuario.class, id);
+	}
+	
+	public List<Usuario> listarUsuarios(){
+		Query query = getEM().createQuery("From Usuarios", Usuario.class);
+		return query.getResultList();
+	}
+	
+	public void excluir(Long id){
+		Usuario usuario = getEM().getReference(Usuario.class, id);
+		getEM().remove(usuario);
+	}
+	
+	private EntityManager getEM() {
+		EntityManager em = JpaUtil.getEntityManager();
+		return em;
+	}
+
+}
---
+++ EscolaNotpadSA/src/br/escolanotpad/sc/model/entity/Usuario.java
@@ -0,0 +1,5 @@
+package br.escolanotpad.sc.model.entity;
+
+public class Usuario {
+
+}
